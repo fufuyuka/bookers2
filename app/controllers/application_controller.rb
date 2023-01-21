4f96e3873ea3debe479d78103b05955849906bea
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-01-21 09:03:34 +0000

    [add]アソシエーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    root_path ##users/:idに修正
+    user_path
   end
 
   def after_sign_out_path_for(resource)
@@ -11,7 +12,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def configure_permitted_parameters
+    def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    end
+    
 end
