de206e37029e60c25450dd2434c3e00a1286dcd0
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-01-22 09:09:19 +0000

    [add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,39 @@
 class BooksController < ApplicationController
   
   def new
+    @book = Book.new
+  end
+  
+  # 投稿データ保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
   end
   
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @post_image.destroy
+    redirect_to books_path
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
   
-  resources :books, only: [:index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :create]
   resources :users, only: [:index, :show, :edit]
   get 'homes/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
