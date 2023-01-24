21819e2b36e3b03983622f1162c14d94d8188913
EC2 Default User <ec2-user@ip-172-31-3-147.ap-northeast-1.compute.internal>
2023-01-24 09:13:57 +0000

    [add]bar bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
   
+  def new
+    
+  end
+  
   def index
     @newbook = Book.new
     @users = User.all
